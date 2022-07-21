25869a068edf1d93efac82efcb41e9a8604308e6
Rui Costa <ruifvcosta@live.com>
2022-07-21 18:43:42 +0100

    Initial config to deploy as service on windows

--- RFVC.M3UFilter.API/Program.cs
+++ RFVC.M3UFilter.API/Program.cs
@@ -1,4 +1,5 @@
 using RFVC.M3UFilter.API;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
 //var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseWindowsService();
 
 var app = builder.Build();
 
